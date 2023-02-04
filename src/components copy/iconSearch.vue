145466ce3abe3c92995280d5f3beaa28256ad0b5
Wabu-K <KobayashiKenta@lifull.com>
2023-02-04 23:19:08 +0900

    initial init

---
+++ src/components copy/iconSearch.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    fill="none"
+    viewBox="0 0 24 24"
+    :stroke="iconColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+    iconColor: {
+      type: String,
+      default: "currentColor",
+    },
+  },
+};
+</script>
