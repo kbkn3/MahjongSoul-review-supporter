78a75217672ce73217dea87b6fd374a829439711
kbkn3 <ken0421wabu@gmail.com>
2026-08-26 02:38:04 +0900

    v1.5.2リリース: バージョンを1.5.2に更新
    package-lock.json が 1.4.1 のまま 1.5.0 / 1.5.1 の2リリース分
    取り残されていたため、あわせて是正する。

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     modules: ["@wxt-dev/module-vue"],
     manifest: {
         name: "__MSG_appName__",
-        version: "1.5.1",
+        version: "1.5.2",
         description: "__MSG_appDesc__",
         author: "kbkn",
         default_locale: "en",
